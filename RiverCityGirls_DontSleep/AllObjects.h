2474c687fbbf2790db27f022f4631cb71e0c9a6d
Andy-ddo <wls6263@naver.com>
2021-02-24 11:20:12 +0900

    retouch

--- RiverCityGirls_DontSleep/AllObjects.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "StandingObj.h"
-#include "BrokenObj.h"
-#include "ItemObj.h"
-#include "Weapon.h"
--- RiverCityGirls_DontSleep/DeadScene.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Scene.h"
-class DeadScene : public Scene
-{
-};
-
